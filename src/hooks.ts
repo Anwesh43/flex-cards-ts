de63796065a3ab7432b2d986bfd73f0e5cc59248
Anwesh43 <anweshthecool0@gmail.com>
2021-03-28 21:18:26 +0530

    adding useStyle hook

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,4 +1,4 @@
-import {useState, useEffect} from 'react'
+import {useState, useEffect, CSSProperties} from 'react'
 
 const delay : number = 20 
 const scGap : number = 0.02 
@@ -35,7 +35,57 @@ export const useDimension = () => {
             setH(window.innerHeight)
         }
         return () => {
+            window.onresize = () => {
 
+            }
         }
     })
+}
+
+export const useStyle = (i : number, w : number, h : number, scale : number) => {
+    const size : number = Math.min(w, h) / 10 
+    return {
+        getChildStyle() : CSSProperties {
+            const width : string = `${size}px`
+            const height : string = `${size}px`
+            const display : string = 'flex'
+            const justifyContent : string = 'center'
+            const alignItems : string = 'center'
+            const background : string = 'teal'
+            return {
+                width,
+                height, 
+                display,
+                justifyContent, 
+                alignItems,
+                background 
+            }
+        },
+        getBlockStyle() : CSSProperties {
+            const width : string = `${size}px`
+            const height : string = `${size}px`
+            const position = 'absolute'
+            const top : string = `${(h - size) * scale}px`
+            const left : string = `${size * i}px`
+            const background : string = 'teal'
+            return {
+                width, 
+                height, 
+                position, 
+                top, 
+                left, 
+                background 
+            }
+        },
+        getParentStyle() : CSSProperties {
+            const height = `${size}px`  
+            const width = `${size * i}px`
+            const display = 'flex'
+            return {
+                width, 
+                height,
+                display
+            }
+        }
+    }
 }
